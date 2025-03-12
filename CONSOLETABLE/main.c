40ebd3ef9af5ccb011fd3c077a19a9926021e013
TsitouhRanjafy <radohery09@gmail.com>
2025-03-12 14:16:46 +0100

    nomage dossier

---
+++ CONSOLETABLE/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * ,,,,,,,,
+ */
+
+
+int main(void){
+    char TopLeft ="┌",TopRight= "┐",BottomLeft= "└",BottomRight= "┘";
+    char Vertical= "│",Horizontal= "─",Center= "┼",CenterTop= "┬",CenterBottom="┴";
+
+    
+    // int *tables = malloc(sizeof(int) * 3);
+    // int a = 13;
+    // tables[0] = &a;
+    
+    // printf("%d : %d",*(&tables+1),tables[0]);
+    return EXIT_SUCCESS;
+}
