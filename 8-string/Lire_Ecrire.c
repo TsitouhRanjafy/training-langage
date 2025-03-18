f7877a5a88aca13ab47d06bab61dafe987c647f8
TsitouhRanjafy <radohery09@gmail.com>
2025-03-18 07:08:03 +0100

    lire phrase

---
+++ 8-string/Lire_Ecrire.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+
+
+int main(void){
+
+    char chaine[8];
+    unsigned long long  n = 64;
+
+    
+
+    return EXIT_SUCCESS;
+}
--- 8-string/main.c
+++ 8-string/main.c
@@ -3,15 +3,50 @@
 #include<stdbool.h>
 #include<ctype.h>
 
+
+
+
+bool lire_ligne(char * chaine, size_t max){
+    size_t i;
+
+    for (i = 0; i < max; i ++){
+        char c;
+        if (scanf("%c",&c) != 1){
+            return false;
+        } else if (c == '\n'){
+            break;
+        }
+        chaine[i] = c;
+    }
+
+    chaine[i] = '\0';
+    return true;
+}
+
 int main(void){
 
 
     char c = 'o';
+    char mot[255];
     bool boolean;
 
     boolean = isalnum(c);
 
-    printf("%d",boolean);
+    printf("%d\n",boolean);
+
+    printf("Quel est votre nom? >");
+    if (scanf("%254s",mot) != 1){
+        printf("Erreur lors de la saisie\n");
+        return EXIT_FAILURE;
+    }
+    printf("Bien le bonjour %s \n",mot);
+
+    while (getchar() != '\n'); // La condition != '\n' signifie "lire et ignorer les caractères jusqu'à atteindre un saut de ligne (\n)".
+    printf("Entrer un phrase de 254 caractère max >");
+    
+    lire_ligne(mot,sizeof(mot));
+
+    printf("\n\t      %s",mot);
     return EXIT_SUCCESS;
 }
 
