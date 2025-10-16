1a8dcc135e42cb0514660ff0cd181fc7cecce761
TsitouhRanjafy <radohery09@gmail.com>
2025-10-16 10:53:43 +0300

    generic constraint

--- typescript/lesson/1-function/generic-function/main.ts
+++ typescript/lesson/1-function/generic-function/main.ts
@@ -9,6 +9,7 @@ console.log(parsed); // Integer Array
 console.log(arrayStr); // string Array
 
 
+/* Generic Constraint */
 function longest<Type extends { length: number }>(a: Type, b: Type) {
     if (a.length >= b.length) {
         return a;
@@ -20,3 +21,18 @@ function longest<Type extends { length: number }>(a: Type, b: Type) {
 const longerArray = longest([1, 2], [1, 2, 3]); // longerArray is of type 'number[]'
 const longerString = longest("alice", "bob"); // longerString is of type 'alice' | 'bob'
 const notOK = longest(10, 100); // Error! Numbers don't have a 'length' property
+
+
+interface Lengthwise {
+  length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+  // Now we know it has a .length property, so no more error
+  console.log(arg.length);
+
+  return arg;
+}
+
+loggingIdentity(3); // Error, number doesn't have a .length property
+loggingIdentity({ length: 10, value: 3 }); // OK
