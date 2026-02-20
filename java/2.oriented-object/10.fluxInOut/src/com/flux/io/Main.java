79be71b872fcc71fcaac067ece10065ebadf498a
TsitouhRanjafy <radohery09@gmail.com>
2026-02-20 14:45:33 +0300

    read the file and directory

---
+++ java/2.oriented-object/10.fluxInOut/src/com/flux/io/Main.java
@@ -0,0 +1,33 @@
+package com.flux.io;
+import java.io.File;
+
+public class Main {
+   public static void main(String[] args){
+       File f = new File("Text.txt");
+       System.out.println("Chemin absolu du fichier: " + f.getAbsolutePath());
+       System.out.println("Nom du fichier : " + f.getName());
+       System.out.println("Est-ce qu'il existe ? " + f.exists());
+       System.out.println("Est-ce un répertoire ? " + f.isDirectory());
+       System.out.println("Est-ce un fichier ? " + f.isFile());
+
+       System.out.println("Affichage des lecteurs à la racine du PC : ");
+       for(File file : f.listRoots()) {
+           System.out.println(file.getAbsolutePath());
+           try {
+               int i = 1;
+               for(File nom : file.listFiles()){
+                   //S'il s'agit d'un dossier, on ajoute un "/"
+                   System.out.print("\t\t" + ((nom.isDirectory()) ? nom.getName()+"/" : nom.getName()));
+
+                   if((i%4) == 0){
+                       System.out.print("\n");
+                   }
+                   i++;
+               }
+               System.out.println("\n");
+           } catch (NullPointerException e) {
+               throw new RuntimeException(e.getMessage());
+           }
+       }
+   }
+}
